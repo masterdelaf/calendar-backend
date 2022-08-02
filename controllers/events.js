c02bdf83cee7ec94888b8495ce2d60bcba3cd19e
Francisco Roselló <masterdelaf1985@gmail.com>
2022-08-02 11:55:59 +0200

    backend terminado

--- controllers/events.js
+++ controllers/events.js
@@ -1,11 +1,13 @@
 const { response } = require('express')
 const Evento = require('../models/Evento')
 
-const getEventos = ( req, res = response ) => {
+const getEventos = async ( req, res = response ) => {
 
-    res.status(201).json({
+    const eventos = await Evento.find()
+                                .populate('user','name')
+    res.json({
         ok: true, 
-        msg: 'getEventos'
+        eventos
     })
 }
 
@@ -34,23 +36,89 @@ const crearEvento = async ( req, res = response ) => {
 
 }
 
-const actualizarEvento = ( req, res = response ) => {
+const actualizarEvento = async ( req, res = response ) => {
 
-    // const { id } = req.params
-    // console.log( id )
+    const eventoId = req.params.id
+    const uid = req.uid
+    
+    try {
+        
+        const evento = await Evento.findById( eventoId )
 
-    res.status(201).json({
-        ok: true, 
-        msg: 'ActualizarEventos'
-    })
+        if( !evento ){
+            return res.status(404).json({
+                ok: false,
+                msg: "Evento no encontrado"
+            })
+        }
+
+        if( evento.user.toString() !== uid ) {
+            return res.status(401).json({
+                ok: false,
+                msg: "No tiene privilegios para editar este evento"
+            })
+        }
+
+        const nuevoEvento = {
+            ...req.body,
+            user: uid
+        }
+
+        const eventoActualizado = await Evento.findByIdAndUpdate( eventoId, nuevoEvento, { new: true } )
+
+        res.json({
+            ok: true,
+            evento: eventoActualizado
+        })
+
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({
+            ok: false,
+            msg: "Hubo un error al actualizar el registro"
+        })
+    }
+
+    
 }
 
-const eliminarEvento = ( req, res = response ) => {
+const eliminarEvento = async ( req, res = response ) => {
 
-    res.status(201).json({
-        ok: true, 
-        msg: 'EliminarEventos'
-    })
+    const eventoId = req.params.id
+    const uid = req.uid
+    
+    try {
+        
+        const evento = await Evento.findById( eventoId )
+
+        if( !evento ){
+            return res.status(404).json({
+                ok: false,
+                msg: "Evento no encontrado"
+            })
+        }
+
+        if( evento.user.toString() !== uid ) {
+            return res.status(401).json({
+                ok: false,
+                msg: "No tiene privilegios para eliminar este evento"
+            })
+        }
+
+        await Evento.findByIdAndDelete( eventoId )
+
+        res.json({
+            ok: true,
+            msg: "Registro eliminado correctamente"
+        })
+
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({
+            ok: false,
+            msg: "Hubo un error al actualizar el registro"
+        })
+    }
 }
 
 module.exports = {
