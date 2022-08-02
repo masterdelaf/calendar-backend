522d6a2bd68f091ef9954d199b03caee0104ef3c
Francisco Roselló <masterdelaf1985@gmail.com>
2022-08-02 10:50:30 +0200

    desde Marbella

--- controllers/events.js
+++ controllers/events.js
@@ -1,4 +1,5 @@
 const { response } = require('express')
+const Evento = require('../models/Evento')
 
 const getEventos = ( req, res = response ) => {
 
@@ -8,12 +9,29 @@ const getEventos = ( req, res = response ) => {
     })
 }
 
-const crearEvento = ( req, res = response ) => {
+const crearEvento = async ( req, res = response ) => {
 
-    res.json({
-        ok: true, 
-        msg: 'CrearEventos'
-    })
+    const evento = new Evento( req.body )
+
+    try {
+
+        evento.user = req.uid
+        
+        const eventoGuardado = await evento.save()
+
+        res.json({
+            ok: true, 
+            evento: eventoGuardado
+        })
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            ok: false,
+            msg: 'Hubo un problema al crear el evento'
+        })
+    }
+
 }
 
 const actualizarEvento = ( req, res = response ) => {
